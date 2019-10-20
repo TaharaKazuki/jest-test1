a3899a43573d2a8fa16468a2933f2ae5b5de5b06
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-20 20:09:32 +0900

    [update]

--- jotto-redux/src/App.js
+++ jotto-redux/src/App.js
@@ -1,1 +1,7 @@
-import React, { } from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './App.css'
+
+export class UnconnectedApp extends Component {
+  
+}
